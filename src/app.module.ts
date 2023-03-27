e9570ff4500dab1738af3c27c8d8bd631b2775ab
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2023-03-27 13:06:34 +0300

    create simple 'users' CRUD using local postgres database

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234',
+      database: 'users',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class AppModule {}
